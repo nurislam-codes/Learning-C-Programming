5c44430ab9e46615524c9eaa2a08f983a69dac8f
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-04-29 00:19:00 +0600

    Added float practice code with decimal formatting

---
+++ datatype_float.c
@@ -0,0 +1,9 @@
+//Learning float data type and controlling decimal places
+#include <stdio.h>
+int main()
+{
+    float grevity=9.8,num1=1.2;
+    printf( " Earth grevity was =%.1f\n" ,grevity);
+    printf( "\n And the num1=%.1f\n" ,num1);
+    return 0;
+}
