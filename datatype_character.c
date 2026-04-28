5bae83d8d67836fac71b31c1928da82c36754fe5
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-04-29 00:35:14 +0600

    Added character datatype practice with my blood group logic

---
+++ datatype_character.c
@@ -0,0 +1,8 @@
+//Learning char data type by printing my blood group
+#include<stdio.h>
+int main()
+{
+    char ch='B',sign='+';
+    printf( "My blood group is =%c%c \n",ch,sign);
+    return 0;
+}
