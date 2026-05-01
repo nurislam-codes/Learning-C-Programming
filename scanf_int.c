4cf104237f6cfa40454df4d9980fa43fc69d66ac
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-05-01 23:46:16 +0600

    Practice taking integer input from user using scanf

---
+++ scanf_int.c
@@ -0,0 +1,15 @@
+// Practice: Taking integer input from user using scanf
+#include<stdio.h>
+int main()
+{
+     // Declaring an integer variable to store age
+     int age;
+     // Asking the user for input
+     printf( "Inter your age =");
+     // Taking integer input from user using scanf
+     scanf( "%d",&age);
+     // Printing the output with a message
+     printf( " wow! your age is %d\n",age);
+     return 0;
+
+}
