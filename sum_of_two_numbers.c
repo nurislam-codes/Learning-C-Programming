2ea538e2c8bb36406f7037dae512803fbf71eec5
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-05-06 21:32:12 +0600

    Practice basic arithmetic operator sum

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,9 @@
+// Topic: Basic Arithmetic Operator (Sum of two numbers)
+#include <stdio.h>
+int main() 
+{
+    int a = 10, b = 20, sum;
+    sum = a + b;
+    printf("Sum is: %d", sum);
+    return 0;
+}
