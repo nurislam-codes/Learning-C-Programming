45edc525a116dc69afccb2af7c7c468834b4e700
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-05-07 22:02:24 +0600

    convert ASCII value into character

---
+++ ascii_value_to_char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    // Declaring an integer variable to store ASCII value
+     int num;
+     // Prompting the user to enter an ASCII value
+     printf( "Enter any ASCII value:");
+     scanf( "%d",&num);
+     // Printing the result as a character using %c
+     printf(" The ASCII charcter is :%c",num);
+     return 0;
+}
+
