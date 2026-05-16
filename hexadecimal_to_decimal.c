f516a294b9fc16e6cc3e83715db6a4536b15fcb3
Md. Nur Islam <nurislam.inbox@gmail.com>
2026-05-17 02:19:40 +0600

    Add program to convert Hexadecimal to Decimal

---
+++ hexadecimal_to_decimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    // Inputting a Hexadecimal number from user
+    printf( "Enter any Hexadecimal number :");
+    scanf("%x",&number);
+  // Converting Hexadecimal to Decimal
+    printf(" Decimal number= %d\n", number);
+    return 0;
+}
+
